4cce3b4d7fbf98fb093c56667608df9d8ad6bd2e
Jesse Duncan <jwduncan88@gmail.com>
2020-02-12 17:59:02 -0500

    initial commit

---
+++ marketcheck.py
@@ -0,0 +1,301 @@
+from tkinter import Tk
+from time import sleep
+from decimal import *
+from threading import Thread
+import pyperclip
+
+
+r = Tk()
+
+r.withdraw()
+
+
+class Main:
+    def __init__(self):
+        self.capital = 1000
+        self.order_count = 20
+        self.volume_multiplier = 5
+        self.storage = ""
+        self.order_price = 0
+        self.margin_skill = 4
+        # 0 = Buy | 1 = Sell
+        self.mode = 0
+        self.competitive_price = 0
+
+        self.setup()
+        self.run()
+
+    def calculate_order_price(self):
+        capital = self.capital * 1000000
+        margin_capital = (
+            capital * Decimal(0.75) ** Decimal(self.margin_skill)
+        ) + capital
+        order_price = Decimal(margin_capital) / Decimal(self.order_count)
+        self.order_price = round(order_price, 2)
+
+    def calculate_profit(self, buy, sell):
+        profit = sell - buy
+        return round(profit, 2)
+
+    def calculate_needed_qty(self, value):
+        qty = self.order_price / value
+        return round(qty, 0)
+
+    def calculate_min_daily_volume(self, value):
+        return value * self.volume_multiplier
+
+    def convert_daily_volume_thousands(self, value):
+        new_value = value / 1000
+        if new_value < 0.01:
+            return "N/A"
+
+        conversion = "{} K".format(round(new_value, 2))
+
+        return conversion
+
+    def convert_daily_volume_millions(self, value):
+        new_value = value / 1000000
+        if new_value < 0.01:
+            return "N/A"
+
+        conversion = "{} M".format(round(new_value, 2))
+
+        return conversion
+
+    def convert_daily_volume_billions(self, value):
+        new_value = value / 1000000000
+        if new_value < 0.09:
+            return "N/A"
+
+        conversion = "{} B".format(round(new_value, 2))
+        return conversion
+
+    def calculate_min_sell(self, value):
+        margin = Decimal(0.10)
+        margin += 1
+        value_with_margin = value * margin
+        return round(value_with_margin, 2)
+
+    def calculate_max_buy(self, value):
+        margin = Decimal(0.10)
+        margin += 1
+        value_with_margin = value / margin
+        return round(value_with_margin, 2)
+
+    def calculate_competitive_price(self, value):
+        if self.mode == 0:
+            return round(value + Decimal(0.01), 2)
+        else:
+            return round(value - Decimal(0.01), 2)
+
+    def parse_value(self, value):
+        split_value = value.split()
+        price_location = 0
+        loc = 0
+        for location in split_value:
+            if location == "ISK":
+                price_location = loc - 1
+                break
+            loc += 1
+        price = Decimal(split_value[price_location].replace(",", ""))
+        return round(price, 2)
+
+    def get_results(self, data):
+        value = self.parse_value(data)
+        min_sell = self.calculate_min_sell(value)
+        max_buy = self.calculate_max_buy(value)
+        profit = self.calculate_profit(value, min_sell)
+
+        needed_qty = self.calculate_needed_qty(value)
+        min_daily_volume = self.calculate_min_daily_volume(needed_qty)
+        daily_volume_thousands = self.convert_daily_volume_thousands(min_daily_volume)
+        daily_volume_millions = self.convert_daily_volume_millions(min_daily_volume)
+        daily_volume_billions = self.convert_daily_volume_billions(min_daily_volume)
+        competitive_price = self.calculate_competitive_price(value)
+        # self.storage = competitive_price
+        self.competitive_price = competitive_price
+        pyperclip.copy(str(competitive_price))
+
+        divider = "=======" * 10
+        sub_divider = "_______" * 10
+        if self.mode == 0:
+            print(
+                """
+{}\n
+{}\n
+{}\n
+Min Sell:
+    {:,.2f} ISK\n
+Min Profit Per Unit:
+    {:,.2f} ISK\n
+Qty:
+    {:,.0f}\n
+{}\n
+Min Volume\n
+    {:,.0f}\n
+    {}\n
+    {}\n
+    {}\n
+{}\n
+Competitive Price:
+    {}\n
+{}\n
+                """.format(
+                    divider,
+                    divider,
+                    divider,
+                    min_sell,
+                    profit,
+                    needed_qty,
+                    sub_divider,
+                    min_daily_volume,
+                    daily_volume_thousands,
+                    daily_volume_millions,
+                    daily_volume_billions,
+                    sub_divider,
+                    competitive_price,
+                    divider,
+                )
+            )
+        else:
+            print(
+                """
+{}\n
+{}\n
+{}\n
+Competitive Price: 
+    {}\n
+Max Buy Price:
+    {}\n
+{}\n
+                """.format(
+                    divider, divider, divider, competitive_price, max_buy, divider
+                )
+            )
+
+    def get_clip_data(self):
+        while not r.selection_get(selection="CLIPBOARD"):
+            sleep(0.1)
+        result = r.selection_get(selection="CLIPBOARD")
+        return result
+
+    def setup(self):
+        self.capital = Decimal(
+            input("Please enter a starting capital in millions\n default: 1000\n ")
+            or self.capital
+        )
+        self.volume_multiplier = Decimal(
+            input("Please enter a volume multiplier\n default: 5\n ")
+            or self.volume_multiplier
+        )
+
+        self.order_count = Decimal(
+            input("Please enter a desired order count:\n default: 20\n")
+            or self.order_count
+        )
+        self.margin_skill = Decimal(
+            input("Please enter your Margin Trade Skill Level:\n default: 4\n")
+            or self.margin_skill
+        )
+
+        self.calculate_order_price()
+
+    def user_input(self):
+        while True:
+            users_input = input("Please enter an option\n")
+            if users_input == "h" or users_input == "help":
+                print(
+                    """
+    Options:
+        q, Q
+            Quit
+        b
+            Change mode to BUY
+        s
+            Change mode to SELL
+        capital
+            Change capital amount
+        orders
+            Change order count
+        volume
+            Change volume multiplier
+        margin
+            Change margin skill level
+        settings
+            List current settings
+                """
+                )
+            if users_input == "q" or users_input == "Q":
+                print("quitting")
+                quit()
+            if users_input == "b":
+                self.mode = 0
+                print("buy mode\n")
+            if users_input == "s":
+                self.mode = 1
+                print("sell mode\n")
+
+            if users_input == "capital":
+                self.capital = Decimal(
+                    input("Please enter new capital in millions\n") or self.capital
+                )
+
+            if users_input == "orders":
+                self.order_count = Decimal(
+                    input("Please enter a desired order count:\n default: 20\n")
+                    or self.order_count
+                )
+
+            if users_input == "volume":
+                self.volume_multiplier = Decimal(
+                    input("Please enter a volume multiplier\n default: 5\n ")
+                    or self.volume_multiplier
+                )
+            if users_input == "margin":
+                self.margin_skill = Decimal(
+                    input("Please enter your Margin Trade Skill Level:\n default: 4\n")
+                    or self.margin_skill
+                )
+
+            if users_input == "settings":
+                print(
+                    """
+    Current Settings
+    ----------------
+    Captial In Millions:  {}
+    Order Count:          {}
+    Volume Multiplier:    {}
+    Margin Skill Level:   {}
+                        """.format(
+                        self.capital,
+                        self.order_count,
+                        self.volume_multiplier,
+                        self.margin_skill,
+                    )
+                )
+
+    def clip_data(self):
+        t1 = Thread(target=self.user_input)
+        t1.start()
+        while True:
+            sleep(0.1)
+            result = self.get_clip_data()
+            if result == self.storage:
+                sleep(0.5)
+                continue
+            if result == str(self.competitive_price):
+                sleep(0.5)
+                continue
+            self.storage = result
+            try:
+                self.get_results(result)
+            except Exception as e:
+                continue
+
+            print("\nPlease enter an option")
+
+    def run(self):
+        self.clip_data()
+
+
+Main()
